4772304f141d4049015910e1ad370f0251e1e756
edwin <147580387+DeemzSaputraaa@users.noreply.github.com>
2024-11-10 21:12:07 +0700

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ export const Hero = () => {
 
   return (
     <section
-      style={{
-        backgroundImage: `url(${gedung})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        // opacity: 0.5, // Atur transparansi di sini
-      }}
-      className=" w-screen flex justify-left items-left bg-bgContent mb-[28vw] md:mb-[18vw] lg:mb-[10vw] xl:mb-[13vw] 2xl:mb-60 hero-bg-gradient pb-24 sm:pb-32 md:pb-44 lg:pb-0"
+      // style={{
+      //   backgroundImage: `url(${gedung})`,
+      //   backgroundSize: "cover",
+      //   backgroundPosition: "center",
+      //   // opacity: 0.5, // Atur transparansi di sini
+      // }}
+      // className=" w-screen flex justify-left items-left bg-bgContent mb-[28vw] md:mb-[18vw] lg:mb-[10vw] xl:mb-[13vw] 2xl:mb-60 hero-bg-gradient pb-24 sm:pb-32 md:pb-44 lg:pb-0"
       className="relative w-full min-h-screen flex items-center bg-blue-200 hero-bg-gradient px-4 sm:px-6 lg:px-8"
       id="home"
     >
